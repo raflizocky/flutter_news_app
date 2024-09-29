90d48cd1137477d2a3a93cc3074d5144b2d8626c
rafli zocky <raflizocky@gmail.com>
2024-09-29 11:01:40 +0700

    secure api

--- lib/data/network/api_service.dart
+++ lib/data/network/api_service.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_news_app/data/model/article.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiService {
-  static const String _baseUrl = 'https://newsapi.org/v2/';
-  static const String _apiKey = '3517e966b0284d63ba6918b9c5831f9e';
+  static String get _baseUrl => dotenv.env['NEWS_BASE_URL'] ?? '';
+  static String get _apiKey => dotenv.env['NEWS_API_KEY'] ?? '';
   static const String _country = 'us';
 
   Future<ArticlesResult> topHeadlines() async {
